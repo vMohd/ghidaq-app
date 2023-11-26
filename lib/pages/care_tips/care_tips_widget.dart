72da5e2104af4d0a8a6ec9d343ef1caae4f03c85
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-26 12:36:49 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/care_tips/care_tips_widget.dart
+++ lib/pages/care_tips/care_tips_widget.dart
@@ -75,57 +75,53 @@ class _CareTipsWidgetState extends State<CareTipsWidget> {
         centerTitle: true,
         elevation: 2.0,
       ),
-      body: SafeArea(
-        top: true,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsetsDirectional.fromSTEB(14.0, 16.0, 0.0, 12.0),
-                    child: Text(
-                      'Flowers Care Instructions: ',
-                      style:
-                          FlutterFlowTheme.of(context).headlineMedium.override(
-                                fontFamily: 'Outfit',
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.w600,
-                              ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        child: Padding(
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              8.0, 0.0, 0.0, 12.0),
-                          child: SelectionArea(
-                              child: Text(
-                            '- Replenish the water frequently. \n- Change the water entirely every 2-3 days.\n- Trim at least a half inch of stem off your flowers before you put them in a vase and each time you change the water.\n- Keep your flowers away from heat and bright light.\n- Avoid sitting your flowers beside ripening fruit or vegetables, especially bananas, and apples.\n\nNote: Flowers can last up to 7 days if the above care instructions are followed and well maintained, the color of the flowers may vary depending on the season.',
-                            style:
-                                FlutterFlowTheme.of(context).bodyLarge.override(
-                                      fontFamily: 'Readex Pro',
-                                      fontSize: 15.0,
-                                      lineHeight: 2.0,
-                                    ),
-                          )),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsetsDirectional.fromSTEB(14.0, 16.0, 0.0, 12.0),
+                  child: Text(
+                    'Flowers Care Instructions: ',
+                    style: FlutterFlowTheme.of(context).headlineMedium.override(
+                          fontFamily: 'Outfit',
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
                         ),
-                      ),
-                    ],
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 0.0, 12.0),
+                        child: SelectionArea(
+                            child: Text(
+                          '- Replenish the water frequently. \n- Change the water entirely every 2-3 days.\n- Trim at least a half inch of stem off your flowers before you put them in a vase and each time you change the water.\n- Keep your flowers away from heat and bright light.\n- Avoid sitting your flowers beside ripening fruit or vegetables, especially bananas, and apples.\n\nNote: Flowers can last up to 7 days if the above care instructions are followed and well maintained, the color of the flowers may vary depending on the season.',
+                          style:
+                              FlutterFlowTheme.of(context).bodyLarge.override(
+                                    fontFamily: 'Readex Pro',
+                                    fontSize: 15.0,
+                                    lineHeight: 2.0,
+                                  ),
+                        )),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
